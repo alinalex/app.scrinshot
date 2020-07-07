89ba71651df1d92c2564c000dca1313f1a12155f
Alin Rauta <alinrauta@Alins-MacBook-Pro.local>
2020-07-07 17:22:49 +0300

    Changed index.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const path = require('path')
-const { createProxyMiddleware } = require('http-proxy-middleware')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.use(
-  '/api',
-  createProxyMiddleware({ target: 'https://api.scrinshot.xyz/api', changeOrigin: true }),
-)
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
